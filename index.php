eff2a8420325eefeb22e85e5572c1632dc7fb72a
Izïa <izia.crinier@gmail.com>
2022-07-07 21:26:46 +0200

    add directories

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 
 <main id="site-main"></main>
 
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae excepturi quia vero vitae voluptatum! Aliquid asperiores atque esse expedita iste magnam minus nostrum quibusdam sit veritatis. Animi aperiam consequatur possimus.
+
 <?php get_footer(); ?>
