a9f4c3a802199b1063d8edfa55b50cfe64319be7
Yohan Centanni <thorbenliya@gmail.com>
2022-07-21 11:54:21 +0200

    integration of blog page

--- template-parts/post-list.php
+++ template-parts/post-list.php
@@ -4,35 +4,49 @@
 
 	$args = [
 		'post_type' => 'post',
-		'posts_per_page' => 2,
+		'posts_per_page' => 4,
 		'post_status' => 'publish',
 		'paged' => $paged
 	];
 
 	$the_query = new WP_Query($args);
 
-	if($the_query->have_posts()){
-		echo '<ul class="post-list">';
-		while($the_query->have_posts()){
-			$the_query->the_post();
-			echo '<li><a href="' . get_permalink($post->ID) . '">';
-			echo '<article>';
-			echo '<img src= "'.get_the_post_thumbnail_url($post). '"/>';
-			echo '<h1>' . $post->post_title . '</h1>';
-            echo '<p>'.wp_strip_all_tags($post->post_content) .'</p>';
-			echo '</article></a></li>';
-		}
-		echo '</ul>';
-	}
-	echo '<div class="post-list-pagination">';
-	echo paginate_links([
-		'total' => $the_query->max_num_pages,
-		'current' => $paged
-	]);
-	echo '</div>';
 	?>
 </div>
 
+
+<div class="row row-cols-1 row-cols-md-2 g-4">
+
+
+			<?php while($the_query->have_posts()){ 
+					$the_query->the_post();
+				?>
+
+				<div class="col">
+					<div class="card">
+						<div class="img-container">
+							<img src= "<?= get_the_post_thumbnail_url($post)?> ">
+						</div>
+						<div class="card-body">
+							<h5 class="card-title" style="margin: 1.5rem 0"><?= $post->post_title ?></h5>
+							<p class="card-text">
+								<?= wp_strip_all_tags($post->post_content)  ?>
+							</p>
+						</div>
+					</div>
+				</div>
+			<?php } ?>
+
+			<?php
+
+					echo '<div class="post-list-pagination">';
+					echo paginate_links([
+						'total' => $the_query->max_num_pages,
+						'current' => $paged
+					]);
+					echo '</div>';
+			?>
+</div>
 
 
 
--- template-parts/post-tiny-list.php
+++ template-parts/post-tiny-list.php
@@ -1,5 +1,4 @@
-<div class="post-list-wrapper">
-	<?php
+<?php
 	$paged = (get_query_var('paged')) ? absint( get_query_var( 'paged' ) ) : 1;
 
 	$args = [
@@ -10,24 +9,30 @@
 	];
 
 	$the_query = new WP_Query($args);
-
-	if($the_query->have_posts()){
-		echo '<ul class="post-list">';
-		while($the_query->have_posts()){
-			$the_query->the_post();
-			echo '<li><a href="' . get_permalink($post->ID) . '">';
-			echo '<article>';
-			echo '<img src= "'.get_the_post_thumbnail_url($post). '"/>';
-			echo '<h1>' . $post->post_title . '</h1>';
-            echo '<p>'.wp_strip_all_tags($post->post_content) .'</p>';
-			echo '</article></a></li>';
-		}
-		echo '</ul>';
-	}
-	echo '</div>';
 	?>
-</div>
 
+
+	<div>
+	<h5>Recent posts</h5>
+
+	
+	<?php while($the_query->have_posts()){
+					$the_query->the_post();
+	?>
+	
+						<div class="recent-post">
+							<div></div>
+							<div>
+								<p>
+									<?=  $post->post_title ?>
+								</p>
+								<p>
+									<?=' <time>' . date_i18n('j F Y', get_post_timestamp($post)) . '</time>' ?>
+								</p>
+							</div>
+						</div>
+	<?php } ?>
+ </div>
 
 
 
